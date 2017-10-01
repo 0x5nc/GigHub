276c842df9fef45c5c8547e1dccfc02aa7c0903d
0x5nc <slim.essency@gmail.com>
2017-10-01 21:54:44 +0100

    Implement validation.

---
+++ GigHub/ViewModels/FutureDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), "dd MMM YYYY",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out var dateTime);
+
+            return (isValid && dateTime > DateTime.Now);
+        }
+    }
+}
